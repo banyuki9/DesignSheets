56326b6c85a28f5fbad5662d05987f95efb2d80a
banyuki9 <yuki1999ban@gmail.com>
2022-12-07 00:43:00 +0900

    add: new layout

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+const Layout:React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Layout;
